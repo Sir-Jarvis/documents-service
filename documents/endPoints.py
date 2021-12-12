04045551aad5954118f32f7a0e3446fc99a7dcd5
Sir-Jarvis <sirjarvis@anything.com>
2021-12-12 21:45:20 +0000

    documents deployment commit

--- documents/endPoints.py
+++ documents/endPoints.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.DocumentAPIView.as_view()),
     path('uploadFile/',views.FileUploadView.as_view())
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
